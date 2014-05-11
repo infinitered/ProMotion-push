5f49b45b3144010000f1b9241838f343710bf02a
Jamon Holmgren <jamon@clearsightstudio.com>
2014-05-10 19:06:53 -0700

    Adding more passing specs

--- spec/push_notification_spec.rb
+++ spec/push_notification_spec.rb
@@ -34,4 +34,35 @@ describe "push notifications" do
         @subject.registered_push_notifications.should == types
       end
   end
+
+  it "should return false for was_launched if the app is currently active on screen" do
+    @subject.mock!(:on_push_notification) do |notification, was_launched|
+      was_launched.should.be.false
+    end
+
+    fake_app = Struct.new(:applicationState).new(UIApplicationStateActive)
+    remote_notification = PM::PushNotification.fake_notification.notification
+    @subject.application(fake_app, didReceiveRemoteNotification: remote_notification)
+  end
+
+  it "should return true for was_launched if app was launched from background" do
+    @subject.mock!(:on_push_notification) do |notification, was_launched|
+      was_launched.should.be.true
+    end
+
+    fake_app = Struct.new(:applicationState).new(UIApplicationStateBackground)
+    remote_notification = PM::PushNotification.fake_notification.notification
+    @subject.application(fake_app, didReceiveRemoteNotification: remote_notification)
+  end
+
+  it "should return true for was_launched if the app wasn't running" do
+    @subject.mock!(:on_push_notification) do |notification, was_launched|
+      was_launched.should.be.true
+    end
+
+    launch_options = { UIApplicationLaunchOptionsRemoteNotificationKey => PM::PushNotification.fake_notification.notification }
+    @subject.application(UIApplication.sharedApplication, didFinishLaunchingWithOptions:launch_options )
+  end
+
+
 end
