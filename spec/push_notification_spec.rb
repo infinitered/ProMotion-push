e1ec45a4c2bd6310b1d3f93f6d1bae45ae26144a
David Larrabee <david.larrabee@meyouhealth.com>
2015-01-29 09:38:05 -0500

    Adds test coverage for iOS8 Features

--- spec/push_notification_spec.rb
+++ spec/push_notification_spec.rb
@@ -62,5 +62,80 @@ describe "push notifications" do
     @subject.application(UIApplication.sharedApplication, didFinishLaunchingWithOptions:launch_options )
   end
 
+  describe "on a version lower than iOS 8" do
+    before do
+      @app = IOS7Application.new
+      UIApplication.stub!(:sharedApplication, return: @app)
+    end
 
+    it "should allow registration for push notifications on < iOS 8" do
+      @subject.register_for_push_notifications(:badge)
+      @app.types.should.equal(UIRemoteNotificationTypeBadge)
+    end
+
+    it "should return nil for :register_push_notification_category" do
+      @subject.register_push_notification_category("category", [], {}).should.be.nil
+    end
+  end
+
+  describe "on iOS 8 +" do
+    before do
+      @app = IOS8Application.new
+      UIApplication.stub!(:sharedApplication, return: @app)
+    end
+
+    it "should allow registration of push notifications on iOS 8+" do
+      @subject.register_for_push_notifications(:badge)
+
+      @app.registered_for_notifications.should.be.true
+      @app.settings.types.should.equal(UIRemoteNotificationTypeBadge)
+      @app.settings.categories.count.should.equal(0)
+    end
+
+    it "should work properly when using register_push_notification_category" do
+      @method_call = @subject.register_push_notification_category("my category", [], {})
+      @method_call.class.should.equal(UIMutableUserNotificationCategory)
+      @method_call.identifier.should.equal("my category")
+    end
+
+    it "should take into consideration the categories used with register_push_notification_category" do
+      @subject.register_push_notification_category("my category", [], {})
+      @subject.register_for_push_notifications(:badge)
+      @app.settings.categories.count.should.equal(1)
+      @app.settings.categories.allObjects[0].identifier.should.equal("my category")
+    end
+
+    it "should call on_push_notification_action if its implemented" do
+
+      @subject.mock!(:on_push_notification_action) do |action, notification|
+        notification.should.be.kind_of(ProMotion::PushNotification)
+        notification.alert.should == "Eating Bacon"
+        notification.badge.should == 42
+        notification.sound.should == "jamon"
+        @subject.aps_notification.should == notification
+      end
+
+      notification = ProMotion::PushNotification.fake_notification(alert: "Eating Bacon", badge: 42, sound: "jamon").notification
+      @subject.application(UIApplication.sharedApplication, handleActionWithIdentifier: "my category", forRemoteNotification: notification, completionHandler: -> {})
+    end
+  end
+
+  class IOS7Application
+    attr_accessor :types
+    def registerForRemoteNotificationTypes(types)
+      self.types = types
+    end
+  end
+  class IOS8Application
+    attr_accessor :settings
+    attr_accessor :registered_for_notifications
+
+    def registerUserNotificationSettings(value)
+      self.settings = value
+    end
+
+    def registerForRemoteNotifications
+      self.registered_for_notifications = true
+    end
+  end
 end
